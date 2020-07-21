0c41c73feef6216839d048a26eb396ca22297be1
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2020-07-21 23:18:41 +0200

    Added sanitize_csv.py

---
+++ sanitize_csv.py
@@ -0,0 +1,54 @@
+#! /usr/bin/python3
+
+from sys import argv
+import csv
+
+
+def argparse():
+    global listfile
+    listfile = argv[1]
+    global csvfile
+    csvfile = argv[2]
+
+
+def read_distros():
+    l = set()
+    with open(listfile) as f:
+        for line in f:
+            l.add(line.replace("\n", ""))
+    return l
+
+
+def add_ancestors(distro):
+    global distros
+    for row in csvdata:
+        if row[0] == "N" and row[1] == distro:
+            if row[3] and row[3] not in distros:
+                distros.add(row[3])
+                add_ancestors(row[3])
+            break
+
+
+def remove_comments():
+    global csvdata
+    csvdata = [row for row in csvdata if row[0]
+               and not row[0].startswith("//") and not row[0].startswith("#")]
+
+
+def remove_unneeded_distros():
+    pass
+
+
+if __name__ == "__main__":
+    argparse()
+
+    distros = read_distros()
+
+    with open(csvfile) as f:
+        csvdata = list(csv.reader(f, delimiter=',', quotechar='"'))
+    remove_comments()
+
+    for distro in distros.copy():
+        add_ancestors(distro)
+
+    remove_unneeded_distros()

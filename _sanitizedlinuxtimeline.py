08004dd435748d0539ac4143c59870287bff8f0a
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2020-07-22 14:17:52 +0200

    Bugfix

--- _sanitizedlinuxtimeline.py
+++ _sanitizedlinuxtimeline.py
@@ -1,9 +1,11 @@
-import json, csv
+import json
+import csv
+
 
 def check_existence(distros, csvfile):
     """check_existence(distros, csvfile)
     Return a set of all distributions that are in distros but not in csvfile
-    
+
     Arguments:
     distros - a set of distributions
     csvfile - csv file 
@@ -12,7 +14,7 @@ def check_existence(distros, csvfile):
     with open(csvfile) as f:
         csvdata = list(csv.reader(f, delimiter=',', quotechar='"'))
 
-    s = set() 
+    s = set()
     for distro in distros:
         found = False
         for row in csvdata:
@@ -27,8 +29,9 @@ def check_existence(distros, csvfile):
                         break
                     i += 3
         if not found:
-            s.add(distro) 
+            s.add(distro)
     return s
+
 
 def read_distros(listfile):
     """read_distros(listfile)
@@ -44,6 +47,7 @@ def read_distros(listfile):
                 s.add(line.replace("\n", ""))
     return s
 
+
 def apply_rules(distros, rulefile):
     d = distros.copy()
     with open(rulefile) as f:
@@ -53,4 +57,20 @@ def apply_rules(distros, rulefile):
             d.remove(distro)
             if rules[distro] and rules[distro][0] != "#":
                 d.add(rules[distro])
+    return d
+
+
+def add_ancestors(distro, distros, csvdata):
+    d = distros.copy()
+    for row in csvdata:
+        if row[0] == "N":
+            add = ( row[1] == distro )
+            i = 8
+            while i < len(row) and not add:
+                add = ( row[i] == distro )
+                i+=3
+            if add and row[3] and row[3] not in d:
+                d.add(row[3])
+                d = add_ancestors(row[3], d, csvdata)
+                break
     return d
--- sanitize_csv.py
+++ sanitize_csv.py
@@ -2,56 +2,63 @@
 
 from sys import argv
 import csv
-from _sanitizedlinuxtimeline import read_distros
+from _sanitizedlinuxtimeline import read_distros, add_ancestors, apply_rules
 
 
 def argparse():
     global listfile
     listfile = argv[1]
-    global csvfile
-    csvfile = argv[2]
+    global inputcsv
+    inputcsv = argv[2]
+    global rulefile
+    rulefile = argv[3]
+    global outputcsv
+    outputcsv = argv[4]
 
 
-def add_ancestors(distro):
-    global distros
-    for row in csvdata:
-        if row[0] == "N" and row[1] == distro:
-            if row[3] and row[3] not in distros:
-                distros.add(row[3])
-                add_ancestors(row[3])
-            break
-
-
-def remove_comments():
-    global csvdata
-    csvdata = [row for row in csvdata if row[0]
+def remove_comments(inputcsv):
+    with open(inputcsv) as f:
+        _csvdata = list(csv.reader(f, delimiter=',', quotechar='"'))
+    csvdata = [row for row in _csvdata if row[0]
                and not row[0].startswith("//") and not row[0].startswith("#")]
+    return csvdata
 
 
-def remove_unneeded_distros():
-    global csvdata
-    for row in csvdata.copy():
-        if row[0] == "N" and row[1] not in distros and row[8] not in distros:
+def remove_unneeded_distros(_csvdata):
+    csvdata = _csvdata.copy()
+    for row in _csvdata:
+        if row[0] == "N" and row[1] not in distros:
+            needed = False
+            i = 8
+            while i < len(row) and not needed:
+                if row[i] in distros:
+                    needed = True
+                i += 3
+            if not needed:
+                csvdata.remove(row)
+        elif row[0] == "C" and ( row[2] not in distros or row[4] not in distros):
             csvdata.remove(row)
-        elif row[0] != "N":
+        elif row[0] == "D" and row[1] not in distros:
             csvdata.remove(row)
+    return csvdata
 
 
-def print_csv():
-    for row in csvdata:
-        print(row)
-
+def write_csv(filename, csvdata):
+    with open(filename, "w") as f:
+        csvwriter = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        for row in csvdata:
+            csvwriter.writerow(row)
 
 if __name__ == "__main__":
     argparse()
 
     distros = read_distros(listfile)
-
-    with open(csvfile) as f:
-        csvdata = list(csv.reader(f, delimiter=',', quotechar='"'))
-    remove_comments()
+    distros = apply_rules(distros, rulefile)
+    csvdata = remove_comments(inputcsv)
 
     for distro in distros.copy():
-        add_ancestors(distro)
-
-    remove_unneeded_distros()
+        distros = add_ancestors(distro, distros, csvdata)
+   
+    csvdata = remove_unneeded_distros(csvdata)
+    
+    write_csv(outputcsv, csvdata)

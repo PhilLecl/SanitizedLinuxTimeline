d71403917039ec05153a107c13ece91477b79d01
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2021-03-27 16:05:36 +0100

    Added copyright notices

--- sanitize_csv.py
+++ sanitize_csv.py
@@ -1,5 +1,21 @@
 #! /usr/bin/python3
 
+# SanitizedLinuxTimeline: Reduce the Linux Distribution Timeline to the most popular distributions.
+# Copyright (C) 2020-2021  Philipp Leclercq
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import sys
 import csv
 from _sanitizedlinuxtimeline import read_distros, add_ancestors, apply_rules, remove_comments, write_csv, remove_unneeded_distros
@@ -29,7 +45,7 @@ if __name__ == "__main__":
 
     for distro in distros.copy():
         distros = add_ancestors(distro, distros, csvdata)
-    
+
     print(len(distros))
     print(distros)
     csvdata = remove_unneeded_distros(csvdata, distros)
